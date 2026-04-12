0e7c493a73cd0f2c77e4847663385aeeb67eae46
Everton Rocha <imevertonrch@gmail.com>
2026-04-12 01:10:25 -0300

    seed

--- src/main/resources/seed.sql
+++ src/main/resources/seed.sql
@@ -1,10 +1,17 @@
 INSERT INTO tb_cozinha (nome) VALUES ('BRASILEIRA');
 INSERT INTO tb_cozinha (nome) VALUES ('MEXICANA');
 
-
 INSERT INTO tb_restaurante (nome, taxa_frete, cozinha_id) VALUES ('coma bem', 23.00, 1);
 INSERT INTO tb_restaurante (nome, taxa_frete, cozinha_id) VALUES ('tacos mexicanos', 15.00, 2);
 INSERT INTO tb_restaurante (nome, taxa_frete, cozinha_id) VALUES ('algum restaurante', 15.00, 2);
 INSERT INTO tb_restaurante (nome, taxa_frete, cozinha_id) VALUES ('outro restaurante', 15.00, 1);
+
+INSERT INTO tb_forma_pagamento (descricao, restaurante_id) VALUES ('PIX', 2);
+INSERT INTO tb_forma_pagamento (descricao, restaurante_id) VALUES ('PIX', 1);
+INSERT INTO tb_forma_pagamento (descricao, restaurante_id) VALUES ('BOLETO', 2);
+INSERT INTO tb_forma_pagamento (descricao, restaurante_id) VALUES ('BOLETO', 4);
+
+INSERT INTO tb_estado (nome) VALUES ('Rio de Janeiro');
+INSERT INTO tb_cidade (nome, estado_id) VALUES ('Niteroi', 1);
 
 

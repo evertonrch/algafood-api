62fbf036ea9dd04a3253fccb22ea5f53c42ef30e
Everton Rocha <imevertonrch@gmail.com>
2026-04-12 01:10:13 -0300

    definindo controller de estados

---
+++ src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Estado;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+
+    private final EstadoRepository estadoRepository;
+
+    public EstadoController(EstadoRepository estadoRepository) {
+        this.estadoRepository = estadoRepository;
+    }
+
+    @GetMapping
+    public List<Estado> listar() {
+        return estadoRepository.buscarTodos();
+    }
+}

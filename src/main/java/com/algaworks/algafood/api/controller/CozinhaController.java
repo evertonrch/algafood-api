447c807fdfa72d56269c991d2faad4b2a21fd6f7
Everton Rocha <imevertonrch@gmail.com>
2026-04-12 02:08:19 -0300

    representando o recurso em json e xml

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -2,6 +2,11 @@ package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +23,21 @@ public class CozinhaController {
         this.cozinhaRepository = cozinhaRepository;
     }
 
-    @GetMapping
-    public List<Cozinha> listar() {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Cozinha> listarJSON() {
         return cozinhaRepository.buscarTodos();
     }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE) // content negotiation
+    public List<Cozinha> listarXML() {
+        return cozinhaRepository.buscarTodos();
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    public ProblemDetail handleNotAcceptable(HttpMediaTypeNotAcceptableException ex) {
+        ProblemDetail detail = ProblemDetail.forStatus(HttpStatus.NOT_ACCEPTABLE);
+        detail.setDetail("nao existe negocicacao com esse media type.");
+        return detail;
+    }
+
 }
